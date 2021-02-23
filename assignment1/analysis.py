3589fbc787bb0623fa690c1e3a6dbf465934c0f0
nielsRocholl <nielsrocholl@gmail.com>
2021-02-23 12:13:43 +0100

    confusion matrix

--- assignment1/analysis.py
+++ assignment1/analysis.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools 
 
 from utils import run_name
 
@@ -56,4 +57,39 @@ def plot_history(stats, variable = 'val_accuracy', save_path = None):
         plt.show()
     else:
         plt.savefig(save_path)
+
+
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
     
--- assignment1/make_plots.py
+++ assignment1/make_plots.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     args = parse_arguments()
     stats = analysis.load_repeats(args.output_path, vars(args), args.repeats)
 
+    cm_plot_labels = ['rock','paper', 'scissors']
+    cm = stats['mean_confusion']
+    analysis.plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
     for var in ["val_accuracy", "val_loss", "accuracy", "loss"]:
         save_path = os.path.join(
             args.output_path,
